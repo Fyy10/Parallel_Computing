411bbbc8edabf803872169047413e8e7a54e2740
JeffFu <1093928135@qq.com>
2021-04-07 21:39:23 +0800

    optim1 finished, refactory required though

--- Lab1/baseline.c
+++ Lab1/baseline.c
@@ -36,18 +36,17 @@ int main(int argc, char *argv[]) {
     }
 
     n = atoi(argv[1]);
-    int n_odd = (n - 1) / 2;
 
     // calculate subarray boundaries
-    low_value = (long long)id * n_odd / p + 1;
-    high_value = (long long)(id + 1) * n_odd / p;
+    low_value = 2 + (long long)id * (n - 1) / p;
+    high_value = 1 + (long long)(id + 1) * (n - 1) / p;
     size = high_value - low_value + 1;
-    // printf("%d: %d, %d, %d\n", id, low_value, high_value, size);
+    // printf("%d, %d, %d\n", low_value, high_value, size);
 
     // make sure all base primes present in process 0
-    proc0_size = n_odd / p;
+    proc0_size = (n - 1) / p;
 
-    if ((2 * proc0_size) < (int)sqrt((double)n)) {
+    if ((2 + proc0_size) < (int)sqrt((double)n)) {
         if (!id) printf("Too many processes\n");
         MPI_Finalize();
         exit(1);
@@ -64,38 +63,27 @@ int main(int argc, char *argv[]) {
 
     for (i = 0; i < size; i++) marked[i] = 0;
     if (!id) index = 0;
-    prime = 3;
+    prime = 2;
 
     do {
-        if (prime * prime > (2 * low_value + 1))
-            first = (prime * prime - 1) / 2 - low_value;
+        if (prime * prime > low_value)
+            first = prime * prime - low_value;
         else {
-            int r = (2 * low_value + 1) % prime;
+            int r = low_value % prime;
             if (!r) first = 0;
-            else {
-                if ((((2 * low_value + 1) / prime + 1) * prime) % 2 == 0)
-                    first = ((((2 * low_value + 1) / prime + 2) * prime) - 1) / 2 - low_value;
-                else
-                    first = ((((2 * low_value + 1) / prime + 1) * prime) - 1) / 2 - low_value;
-            }
+            else first = prime - r;
         }
         for (i = first; i < size; i += prime) marked[i] = 1;
         if (!id) {
             while (marked[++index]);
-            prime = 2 * (index + 1) + 1;
+            prime = index + 2;
         }
         if (p > 1) MPI_Bcast(&prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
     } while (prime * prime <= n);
 
-    // printf("%d: ", id);
     count = 0;
-    for (i = 0; i < size; i++){
-        // print number and marked flag
-        // printf("%d(%d) ", 2 * (low_value + i) + 1, marked[i]);
+    for (i = 0; i < size; i++)
         if (!marked[i]) count++;
-    }
-    // printf("\n");
-
     if (p > 1) MPI_Reduce(&count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     // for serial baseline (num_proc = 1)
     else global_count = count;
@@ -105,7 +93,7 @@ int main(int argc, char *argv[]) {
 
     // print result
     if (!id) {
-        printf("There are %d primes less than or equal to %d\n", global_count + 1, n);
+        printf("There are %d primes less than or equal to %d\n", global_count, n);
         printf("SIEVE (%d) %10.6f\n", p, elapsed_time);
     }
 
